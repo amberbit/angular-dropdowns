acd801fde4b79e0d5971742f09958bc4c68fafc0
Iryna Koroljuk <iryna.koroljuk@amberbit.com>
2014-11-12 17:29:17 +0100

    using span instead of a

--- angular-dropdowns.js
+++ angular-dropdowns.js
@@ -9,7 +9,7 @@ var dd = angular.module('ngDropdowns', []);
 dd.run(['$templateCache', function ($templateCache) {
   $templateCache.put('ngDropdowns/templates/dropdownSelect.html', [
     '<div class="wrap-dd-select">',
-      '<span href="#" class="selected">{{dropdownModel[labelField]}}</span>',
+      '<span class="selected">{{dropdownModel[labelField]}}</span>',
       '<ul class="dropdown">',
         '<li ng-repeat="item in dropdownSelect"',
         ' class="dropdown-item"',
