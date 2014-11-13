ce8d3843032dfef5c4fdd680e3ba74ed8519aa9a
Iryna Koroljuk <iryna.koroljuk@amberbit.com>
2014-11-13 13:50:52 +0100

    placeholder_to_string

--- angular-dropdowns.js
+++ angular-dropdowns.js
@@ -9,7 +9,7 @@ var dd = angular.module('ngDropdowns', []);
 dd.run(['$templateCache', function ($templateCache) {
   $templateCache.put('ngDropdowns/templates/dropdownSelect.html', [
     '<div class="wrap-dd-select">',
-      '<a href="" class="selected">{{dropdownModel[labelField] || dropdownPlaceholder}}</a>',
+      '<a href="" class="selected">{{dropdownModel[labelField] || #{dropdownPlaceholder}}}</a>',
       '<ul class="dropdown">',
         '<li ng-repeat="item in dropdownSelect"',
         ' class="dropdown-item"',
